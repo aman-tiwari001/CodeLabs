561d1073477a85dde0e4d19c14a5af8ae84feda6
Aman Tiwari <amananjalitiwari2007@gmail.com>
2026-01-04 17:56:34 +0530

    Update vite.config.ts

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-// import mkcert from 'vite-plugin-mkcert';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // plugins: [react(), mkcert()],
-  // server: {
-  // 	https: true,
-  // 	host: true,
-  // },
+	plugins: [react()],
+	preview: {
+		host: '0.0.0.0',
+		allowedHosts: ['code-labs.tech'],
+	},
+	server: {
+		allowedHosts: ['code-labs.tech'],
+	},  
 });
